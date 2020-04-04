c5c289e4b175804de2d8f377f4ae88b7aa856dc5
devdevil-production <saudd2gh@gmail.com>
2020-04-04 15:54:49 +0400

    Change active tab link

--- js/tabbed.js
+++ js/tabbed.js
@@ -1,1 +1,11 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  const tabLinks = $(".tabs__item > a");
+  tabLinks.each((_, link) => {
+    $(link).click(function () {
+      const activeLink = $(".tabs__item > a.active");
+      activeLink.removeClass("active");
+
+      $(this).addClass("active");
+    });
+  });
+});
