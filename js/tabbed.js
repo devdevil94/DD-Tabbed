7ae936f4e1472e7be6697716ea5459e5934ae4d6
devdevil-production <saudd2gh@gmail.com>
2020-04-05 11:14:28 +0400

    Change the active tab content

--- js/tabbed.js
+++ js/tabbed.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
+  $(`.tabs__content.active`).first().css("display", "block");
+
   const tabLinks = $(".tabs__item > a");
   tabLinks.each((_, link) => {
     $(link).click(function () {
-      const activeLink = $(".tabs__item > a.active");
-      activeLink.removeClass("active");
+      const activeTabLink = $(".tabs__item > a.active");
+      const targetTabLink = $(this);
 
-      $(this).addClass("active");
+      activeTabLink.removeClass("active");
+      targetTabLink.addClass("active");
+
+      const activeTabContentId = activeTabLink.attr("data-tab-target");
+      const targetTabContentId = targetTabLink.attr("data-tab-target");
+
+      const activeTabContent = $(".tabs__content.active");
+      const targetTabContent = $(`.tabs__content#${targetTabContentId}`);
+
+      activeTabContent.removeClass("active");
+      targetTabContent.addClass("active");
+
+      $(`.tabs__content#${activeTabContentId}`).hide();
+      $(`.tabs__content#${targetTabContentId}`).fadeIn();
     });
   });
 });
